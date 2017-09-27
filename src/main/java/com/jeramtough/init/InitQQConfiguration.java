e622ec39bcc231342bb6af874b00809594ae06d5
JeramTough <1171867004@qq.com>
2017-09-27 19:25:53 +0800

    Signed-off-by: JeramTough <1171867004@qq.com>

--- src/main/java/com/jeramtough/init/InitQQConfiguration.java
+++ src/main/java/com/jeramtough/init/InitQQConfiguration.java
@@ -30,30 +30,37 @@ public class InitQQConfiguration implements ApplicationListener<ContextRefreshed
 	@Override
 	public void onApplicationEvent(ContextRefreshedEvent event)
 	{
+		QQConnectConfig.updateProperties("app_ID", qqConfigurationMapper.getAppId());
+		QQConnectConfig.updateProperties("app_KEY", qqConfigurationMapper.getAppKey());
+		QQConnectConfig 
+				.updateProperties("redirect_URI", qqConfigurationMapper.getRedirectUri());
+		/*Properties properties = new Properties();
 		Resource resource = event.getApplicationContext()
 				.getResource("classpath:qqconnectconfig.properties");
 		try
 		{
+			String path = resource.getURI().getPath();
+			
 			InputStream inputStream = resource.getInputStream();
 			properties.load(inputStream);
 			
 			QQConnectConfig.updateProperties("app_ID", qqConfigurationMapper.getAppId());
 			QQConnectConfig.updateProperties("app_KEY",qqConfigurationMapper.getAppKey());
-			QQConnectConfig.updateProperties("redirect_URI",qqConfigurationMapper.getRedirectUri());
+			QQConnectConfig.updateProperties("app_ID", qqConfigurationMapper.getAppId());
 			
-			/*properties.setProperty("app_ID", qqConfigurationMapper.getAppId());
+			*//*properties.setProperty("app_ID", qqConfigurationMapper.getAppId());
 			properties.setProperty("app_KEY",qqConfigurationMapper.getAppKey());
 			properties.setProperty("redirect_URI",qqConfigurationMapper.getRedirectUri());
 			
 			OutputStream outputStream=new FileOutputStream(new File(path));
-			properties.store(outputStream,"");*/
+			properties.store(outputStream,"");*//*
 			
 			inputStream.close();
 		}
 		catch (IOException e)
 		{
 			e.printStackTrace();
-		}
+		}*/
 	}
 	
 }
